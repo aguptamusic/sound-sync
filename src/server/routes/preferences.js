17b230de02e872948aa062269bb19b4312c569e8
aguptamusic <aguptamusic@gmail.com>
2020-12-05 20:08:53 -0800

    started preferences api call

--- src/server/routes/preferences.js
+++ src/server/routes/preferences.js
@@ -1,3 +1,28 @@
 //called after auth, calls Spotify artists/shows endpoints and stores in db
 const axios = require("axios");
-const pref_url = "https://api.spotify.com/v1/me/top/{type}";
+const artist_url = "https://api.spotify.com/v1/me/top/artists";
+const track_url = "https://api.spotify.com/v1/me/top/tracks";
+
+module.exports = (app) => {
+  app.get("/api/preferences", async (req, res) => {
+    let token = req.query.access_token || null;
+    var artist_res;
+    var track_res;
+    try {
+      //GET top artists
+      artist_res = await axios.get(artist_url, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
+      //GET top tracks
+      track_res = await axios.get(track_url, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  });
+};
