78985c2bc0e93c88b304646e6c43307aac7841a1
Luke Tchang <ltchang@stanford.edu>
2020-12-06 09:59:30 -0800

    added cookie session

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require('cookie-session');
 const config = require("../config/config");
 
 mongoose.connect(config.mongoURI, { useNewUrlParser: true });
 require("./models/user");
 
 const app = express();
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [config.cookieKey]
+  })
+);
+
 require("./routes/auth")(app);
 require("./routes/profile")(app);
 require("./routes/preferences")(app);
 require("./routes/match")(app);
-
 
 app.get("*", (req, res) => {
   res.send("Home Page");
