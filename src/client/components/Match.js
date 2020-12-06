2cec30b0cb8efbd3e8ac1b0a475c8b54b5a2fcc2
aguptamusic <aguptamusic@gmail.com>
2020-12-06 02:40:29 -0800

    changed button size and added animation

--- src/client/components/Match.js
+++ src/client/components/Match.js
@@ -1,15 +1,23 @@
-import { Button } from 'semantic-ui-react';
+import { Button } from "semantic-ui-react";
+import { Icon } from "semantic-ui-react";
+import "../App.css";
 
 function Match() {
-    return (
-        <Button 
-            href="http://localhost:4000/api/matches"
-            className='ui green button'                     
-            color='green'
-        >
-            Recommend Matches!
-        </Button>
-    );
+  return (
+    <div className="Green-Button">
+      <Button
+        animated
+        color="green"
+        href="http://localhost:4000/api/matches"
+        size="massive"
+      >
+        <Button.Content visible>Sound Sync!</Button.Content>
+        <Button.Content hidden>
+          <Icon name="arrow right" />
+        </Button.Content>
+      </Button>
+    </div>
+  );
 }
 
 export default Match;
